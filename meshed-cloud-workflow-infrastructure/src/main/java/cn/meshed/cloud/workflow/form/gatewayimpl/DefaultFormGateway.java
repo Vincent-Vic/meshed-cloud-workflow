6b582e071d9dc2c03d604c4ee0d4e1d15707e237
Vincent Vic <vic.coding@gmail.com>
2023-05-07 22:03:49 +0800

    fix: 允许编辑中的动态表单删除

--- meshed-cloud-workflow-infrastructure/src/main/java/cn/meshed/cloud/workflow/form/gatewayimpl/DefaultFormGateway.java
+++ meshed-cloud-workflow-infrastructure/src/main/java/cn/meshed/cloud/workflow/form/gatewayimpl/DefaultFormGateway.java
@@ -300,7 +300,8 @@ public class DefaultFormGateway implements FormGateway {
         FormDO formDO = getFormDO(formId);
         AssertUtils.isTrue(formDO != null, "表单不存在");
         assert formDO != null;
-        AssertUtils.isTrue(formDO.getStatus() == FormStatusEnum.DISCARD, "表单运行中不允许删除，请废弃后操作");
+        AssertUtils.isTrue(formDO.getStatus() == FormStatusEnum.DISCARD || formDO.getStatus() == FormStatusEnum.EDIT,
+                "表单运行中不允许删除，请废弃后操作");
         return formMapper.deleteById(formId) > 0;
     }
 }
